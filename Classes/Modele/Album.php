bb4e7d0e98c040a532841fa3f0cae7184a7b2482
Nathan.Pigoreau <nathan.pigoreau@etu.univ-orleans.fr>
2024-02-01 12:22:06 +0100

    Ajout d'une première classe Album

---
+++ Classes/Modele/Album.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Album\Type;
+
+final class Album
+{
+    private int $idAlbum;
+
+    private int $idArtiste;
+
+    private String $nomAlbum;
+
+    private date $realeaseYear;
+
+    private int $idGenre;
+
+    private String $image;
+
+    private Array $musiques;
+
+    public function __construct(int $idAlbum, int $idArtiste, String $nomAlbum, date $realseYear, int $idGenre)
+    {
+        $this->idAlbum = $idAlbum;
+        $this->idArtiste = $idArtiste;
+        $this->nomAlbum = $nomAlbum;
+        $this->realeaseYear = $realeaseYear;
+        $this->genre = $genre;
+        $this ->image = None;
+        $this->musiques = [];
+    }
+
+    public function getIdAlbum(): int
+    {
+        return $this->idAlbum;
+    }
+
+    public function getIdArtiste(): int
+    {
+        return $this->idArtiste;
+    }
+
+    public function getNomAlbum(): String
+    {
+        return $this->nomAlbum;
+    }
+
+    public function getRealeaseYear(): date
+    {
+        return $this->realeaseYear;
+    }
+
+    public function getIdGenre(): int
+    {
+        return $this->idGenre;
+    }
+
+    public function getImage(): String
+    {
+        return $this->image;
+    }
+
+    public function getMusiques(): Array
+    {
+        return $this->musiques;
+    }
+
+    public function setImage(String $image): void
+    {
+        $this->image = $image;
+    }
+
+    public function setMusiques(Array $musiques): void
+    {
+        $this->musiques = $musiques;
+    }
+
+    public function addMusique(Musique $musique): void
+    {
+        $this->musiques[] = $musique;
+    }
+
+    public function removeMusique(Musique $musique): void
+    {
+        $key = array_search($musique, $this->musiques, true);
+        if ($key !== false) {
+            unset($this->musiques[$key]);
+        }
+    }
+
+    public function render()
+    {
+        $html = '<div class="album">';
+        $html .= '<div class="album-image">';
+        $html .= '<img src="' . $this->image . '" alt="' . $this->nomAlbum . '">';
+        $html .= '</div>';
+        $html .= '<div class="album-infos">';
+        $html .= '<h2>' . $this->nomAlbum . '</h2>';
+        $html .= '<h3>' . $this->realeaseYear . '</h3>';
+        $html .= '<h3>' . $this->genre . '</h3>';
+        $html .= '<h3>' . $this->artiste . '</h3>';
+        $html .= '</div>';
+        $html .= '</div>';
+        return $html;
+    }
+}
--- Classes/Provider/DataLoaderSqllite.php
+++ Classes/Provider/DataLoaderSqllite.php
@@ -9,10 +9,35 @@ final class DataLoaderSqlite implements DataLoaderInterface
 
     private \PDO $pdo;
 
-
+    private array $keys = [''];
 
     public function getData(): array
     {
         return $this->data;
+    }
+
+    public function __construct(\PDO $pdo)
+    {
+        $this->pdo = $pdo;
+        $stmt = $pdo->query('SELECT * FROM ARTISTE');
+        $content = $stmt->fetchAll();
+
+        $this->data = array_map(function($v){
+            $d = [];
+            foreach ($v as $key => $value) {
+                if(!in_array($key, $this->keys)) {
+                    continue;
+                }
+                $d[$key] = $value;
+                if ($key === 'choices' && $value) {
+                    $d[$key] = explode(',', $value);
+                }
+            }
+            return $d;
+        }, $contents);
+
+        if (empty($this->data)) {
+            throw new \Exception(sprintf('No question in "%s"', 'sqlite'));
+        }
     }
 }
