648d2e2d74d0b50d74ab3b7b60fc6f0e6c5c3a97
Niveau Samuel <o22204836@info27-14.iut45.univ-orleans.fr>
2024-02-01 12:24:44 +0100

    test

--- Classes/autoloader.php
+++ Classes/autoloader.php
@@ -1,9 +1,24 @@
 <?php
+require_once 'Classes/Debug/dd.php';
+/**
+ * Class Autoloader
+ */
+class Autoloader{
 
-spl_autoload_register(function ($className) {
-    $classPath = str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
-    if (file_exists($classPath)) {
-        require_once $classPath;
+    /**
+     * Enregistre notre autoloader
+     */
+    static function register(){
+        spl_autoload_register(array(__CLASS__, 'autoload'));
     }
-});
-?>
+
+    /**
+     * Inclue le fichier correspondant à notre classe
+     * @param $class string Le nom de la classe à charger
+     */
+    static function autoload($fqcn){
+        $path = str_replace('\\', '/', $fqcn);
+        require 'Classes/' . $path . '.php';
+    }
+
+}
---
+++ Static/templates/base.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gaitunes</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">
+                        <img src="home.png" alt="écran d'accueil"/>
+
+                    </a>
+                </li>
+                <li>
+                    <a href="/playlist"><p>Playlist</p></a>
+                </li>
+                <li>
+                    <a href="/profil"><p>Profil</p></a>
+                </li>
+                <li>
+                    <a href="/logout"><p>Logout</p></a>
+                </li>
+                
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
