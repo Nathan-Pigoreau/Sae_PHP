6a9e30103d41b3e050cf0fbeaddf81e0012b1d41
Malleron Daniel <daniel.malleron@gmail.com>
2024-02-13 14:14:54 +0100

    correction logo

--- Templates/main.php
+++ Templates/main.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/Static/css/main.css">
-    <title>Document</title>
+    <title>Gaitunes</title>
 </head>
 <body>
     <?php include 'base.php'; ?>
