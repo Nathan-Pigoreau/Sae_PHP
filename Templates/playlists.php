68cc286d83a837356580f51e6b8f56886a44af1b
SNIVEAU <odevva@gmail.com>
2024-02-15 11:33:26 +0100

    changement couleur playlist

--- Templates/playlists.php
+++ Templates/playlists.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Static/css/playlist.css">
+
     <title>Ajouter Playlist</title>
 </head>
 <body>
